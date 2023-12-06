7e473bd369ce1054f3e1947b24a2b3accd9f7116
Adam Zielonka <root@adamzielonka.pro>
2023-12-06 21:43:04 +0100

    Return HTML

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
 #[macro_use] extern crate rocket;
 
+use rocket::response::content::RawHtml;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> RawHtml<&'static str> {
+    RawHtml("<html>
+        <head>
+            <title>Server Status</title>
+        </head>
+        <body>
+            <header>
+                <h1>Server Status</h1>
+            </header>
+            <main>
+                <div>
+                    ?
+                </div>
+                <div>
+                    ?
+                </div>
+                <div>
+                    ?
+                </div>
+            </main>
+        </body>
+    </html>")
 }
 
 #[launch]
